1a28d93bc477d86f41d9452d62aef4ff9c129126
Тимур Сасиков <tm@MacBook-Pro-Admin.local>
2023-05-15 23:04:34 +0300

    fix console error

--- src/scripts/asideOpen.js
+++ src/scripts/asideOpen.js
@@ -1,3 +1,6 @@
+import disableMainScroll from "./disableMainScroll";
+
+
 const sidebarOpenAddHandler = (btn, sidebar) => {
     btn.addEventListener('click', () => {
         sidebar.classList.add('sidebar--open');
